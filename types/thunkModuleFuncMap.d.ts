7aece3984f92b4f5af6e3ac9ce0cdeef0eb9d83d
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-10-25 07:15:06 -0400

    update the type of data in setData as Partial<S>

--- types/thunkModuleFuncMap.d.ts
+++ types/thunkModuleFuncMap.d.ts
@@ -1,6 +1,6 @@
 export declare const DEFAULT_THUNK_MODULE_FUNC_MAP: {
     init: <S extends import("./stateTypes").State>(params: import("./init").InitParams<S>, myuuidv4?: () => string) => import("./action").Thunk<S>;
-    setData: <S extends import("./stateTypes").State>(myID: string, data: S) => import("./action").BaseAction;
+    setData: <S extends import("./stateTypes").State>(myID: string, data: Partial<S>) => import("./action").BaseAction;
     remove: <S extends import("./stateTypes").State>(myID: string, isFromParent?: boolean) => import("./action").Thunk<S>;
     addChild: (myID: string, child: import("./stateTypes").NodeMeta) => import("./addRelation").AddRelationAction;
     removeChild: <S extends import("./stateTypes").State>(myID: string, childID: string, childClass: string, isFromChild?: boolean) => import("./action").Thunk<S>;
