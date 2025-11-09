6060cdbe84f3444c8e5f20b8638e6e31fd864a8f
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-11-09 06:00:41 -0500

    throw exception if not register or not <Thunk />

--- src/useThunk.ts
+++ src/useThunk.ts
@@ -12,8 +12,15 @@ export type UseThunk<S extends State, R extends ThunkModuleFunc<S>> = [ClassStat
 export default <S extends State, R extends ThunkModuleFunc<S>>(theDo: ThunkModule<S, R>): UseThunk<S, R> => {
   const { myClass } = theDo
 
-  const { context } = contextMap.theMap[myClass]
+  if (!contextMap.theMap.hasOwnProperty(myClass)) {
+    throw Error(`useThunk: not registering Thunk yet: className: ${myClass}`)
+  }
 
+  const { context } = contextMap.theMap[myClass]
+  const theContext = useContext(context)
+  if (!theContext) {
+    throw Error(`useThunk: unable to useContext (no <Thunk />?) className: ${myClass}`)
+  }
   const { classState, dispatchMap } = useContext(context)
 
   return [classState, dispatchMap as DispatchFuncMap<S, R>]
