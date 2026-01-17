924d378b9cc627022d75f4c1e93059a8529c2b53
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2026-01-18 00:29:49 +0800

    ThunkContext as JSX.Element

--- types/ThunkContext.d.ts
+++ types/ThunkContext.d.ts
@@ -1,7 +1,7 @@
-import { type ReactNode } from 'react';
+import { type JSX } from 'react';
 type Props = {
     classes?: string[];
-    children?: ReactNode;
+    children?: JSX.Element;
 };
-declare const ThunkContext: (props: Props) => string | number | bigint | boolean | Iterable<ReactNode> | Promise<string | number | bigint | boolean | import("react").ReactElement<unknown, string | import("react").JSXElementConstructor<any>> | import("react").ReactPortal | Iterable<ReactNode> | null | undefined> | import("react/jsx-runtime").JSX.Element | null | undefined;
+declare const ThunkContext: (props: Props) => JSX.Element;
 export default ThunkContext;
