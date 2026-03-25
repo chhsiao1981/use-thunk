cbd26aba5dec9331a6c91655b21a3d5cc2a791b8
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2026-03-25 21:24:16 +0800

    children in ThunkContext as JSX.Element or JSX.Element[]

--- types/ThunkContext.d.ts
+++ types/ThunkContext.d.ts
@@ -1,7 +1,7 @@
 import { type JSX } from 'react';
 type Props = {
     classes?: string[];
-    children?: JSX.Element[];
+    children?: JSX.Element | JSX.Element[];
 };
 declare const ThunkContext: (props: Props) => JSX.Element;
 export default ThunkContext;
